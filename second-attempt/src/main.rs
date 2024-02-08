c77bdc4b36cebff306315b93a4e86c32f9674d92
Daniel Oraca <danieloraca@daniels-mbp.home>
2024-02-08 19:24:26 +0000

    types

--- second-attempt/src/main.rs
+++ second-attempt/src/main.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use aws_config::BehaviorVersion;
 use aws_sdk_dynamodb::Client;
 use aws_sdk_dynamodb::types::AttributeValue;
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
 struct Request {
@@ -14,6 +15,65 @@ struct Response {
     req_id: String,
     msg: String,
     my_id: Option<i32>
+}
+
+pub struct SolarSystem {
+    pub id: String,
+    pub name: String,
+    pub type_: String,
+}
+
+impl SolarSystem {
+    pub fn new(id: String, name: String, type_: String) -> Self {
+        SolarSystem {
+            id,
+            name,
+            type_,
+        }
+    }
+}
+
+fn as_string(val: Option<&AttributeValue>, default: &String) -> String {
+    if let Some(v) = val {
+        if let Ok(s) = v.as_s() {
+            return s.to_owned();
+        }
+    }
+    default.to_owned()
+}
+
+impl From<&HashMap<String, AttributeValue>> for SolarSystem {
+    fn from(item: &HashMap<String, AttributeValue>) -> Self {
+        let id = as_string(item.get("id"), &"".to_string());
+        let name = as_string(item.get("name"), &"".to_string());
+        let type_ = as_string(item.get("type"), &"".to_string());
+
+        SolarSystem::new(id, name, type_)
+    }
+}
+
+pub async fn query_by_id(
+    client: &Client,
+    table_name: &str,
+    id: &str,
+) -> Result<Vec<SolarSystem>, Error> {
+    let result = client
+        .query()
+        .table_name(table_name)
+        .key_condition_expression("id = :id")
+        .expression_attribute_values(":id", AttributeValue::S(id.to_string()))
+        .send()
+        .await?;
+
+    if let Some(items) = result.items {
+        let solar_systems = items
+            .iter()
+            .map(|v| v.into())
+            .collect();
+        Ok(solar_systems)
+    } else {
+        Ok(vec![])
+    }
 }
 
 // async fn query_by_id(client: &Client, table_name: String) {
