cc263bd85448d9a92d02ae0884765067c1b45b65
Dan <daniel.oraca@gmail.com>
2024-03-24 20:11:17 +0000

    real threading

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,40 +1,49 @@
-use std::{thread::{self, JoinHandle}, time::Duration};
+use std::{thread::self};
 use csv::Writer;
 use micro_rand::*;
-use std::sync::mpsc::{channel, Sender, Receiver};
+use std::sync::mpsc::{channel, Sender};
 use uuid::Uuid;
 use names::Generator;
+
+struct CsvRow {
+    id: Uuid,
+    name: String,
+    age: u8,
+    email: String,
+}
 
 fn main() {
     let output_file = "output.csv";
     let mut writer = Writer::from_path(output_file).expect("Cannot create csv writer");
-    let mut rows: Vec<&str> = vec![];
 
-    let (tx, rx): (Sender<String>, Receiver<String>) = channel();
+    let (tx, rx): (Sender<Vec<CsvRow>>, _) = channel();
 
-    let thread_fn1 = move || {
-        let mut rows = vec![];
-        let mut r = Random::new(123475765765);
-        let rnd = r.next_int_i32(0, 20000);
+    let num_threads = 4;
+    for _ in 0..num_threads {
+        let tx = tx.clone();
+        thread::spawn(move || {
+            let mut rows = Vec::with_capacity(250_000);
+            let mut r = Random::new(123475765765);
+            //let rnd = r.next_int_i32(0, 20000);
             let mut generator: Generator = Generator::default();
-        for i in 0..100000000 {
-            let id = Uuid::new_v4();
-            let name: String = generator.next().unwrap();
-            let age = 10;
-            let email = format!("{}@gmail.com", name.to_lowercase());
-            rows.push(format!("{},{},{},{},{}", i+1, id, name, age, email));
-        };
+            for _ in 0..250_000 {
+                let id = Uuid::new_v4();
+                let name: String = generator.next().unwrap();
+                let age = r.next_int_i32(18, 99) as u8;
+                let email = format!("{}@gmail.com", name.to_lowercase());
+                let person = CsvRow { id, name, age, email };
 
-        for val in rows {
-            tx.send(val).unwrap();
-        }
-    };
-
-    thread::spawn(thread_fn1);
-
-    for received in rx {
-        writer.write_record(&[received]).expect("Cannot write record");
+                rows.push(person);
+            }
+            let _ = tx.send(rows);
+        });
     }
 
-}
+    drop(tx);
 
+    for received in rx {
+        for record in received {
+                        writer.write_record(&[format!("{},{},{},{}", record.id, record.name, record.age, record.email)]).expect("Cannot write record");
+        }
+    }
+}
