59203c82d2e7baa7e4ce5bf998e92eb866ce6d58
Dan <daniel.oraca@gmail.com>
2024-03-24 20:23:57 +0000

    use Arc

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,7 +1,7 @@
 use std::{thread::self};
 use csv::Writer;
 use micro_rand::*;
-use std::sync::mpsc::{channel, Sender};
+use std::sync::{mpsc, Arc};
 use uuid::Uuid;
 use names::Generator;
 
@@ -16,17 +16,20 @@ fn main() {
     let output_file = "output.csv";
     let mut writer = Writer::from_path(output_file).expect("Cannot create csv writer");
 
-    let (tx, rx): (Sender<Vec<CsvRow>>, _) = channel();
+    let (tx, rx) = mpsc::channel();
+    let tx = Arc::new(tx);
 
-    let num_threads = 4;
+    let num_threads = 6;
+    let max_rows = 1_000_000;
+    let rows_per_thread = max_rows / num_threads;
+
     for _ in 0..num_threads {
-        let tx = tx.clone();
+        let tx = Arc::clone(&tx);
         thread::spawn(move || {
-            let mut rows = Vec::with_capacity(250_000);
+            let mut rows = Vec::with_capacity(rows_per_thread);
             let mut r = Random::new(123475765765);
-            //let rnd = r.next_int_i32(0, 20000);
             let mut generator: Generator = Generator::default();
-            for _ in 0..250_000 {
+            for _ in 0..rows_per_thread {
                 let id = Uuid::new_v4();
                 let name: String = generator.next().unwrap();
                 let age = r.next_int_i32(18, 99) as u8;
