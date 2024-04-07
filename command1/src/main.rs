9792418e16bd10342231124356c89ff854197f9a
Dan <daniel.oraca@gmail.com>
2024-04-07 19:30:07 +0100

    ls command use macro

--- command1/src/main.rs
+++ command1/src/main.rs
@@ -1,20 +1,26 @@
 use colored::*;
-use core::panic;
-use std::process::{Child, Command, Stdio};
+use std::process::{Command, Stdio};
 
 macro_rules! print_line {
     ($line:expr) => {{
         let msg = $line.blue().on_yellow();
         println!("{msg}");
+        println!("{}", "-".repeat(get_screen_size()));
     }};
+}
+
+fn get_screen_size() -> usize {
+    let mut width = 0;
+    if let Some((x, _)) = term_size::dimensions() {
+        width = x;
+    }
+
+    width
 }
 
 fn main() {
     let mut command: Command = Command::new("ls");
     command.arg("-la");
-
-    // let mut child: Child = command.spawn().unwrap();
-    // child.wait().unwrap();
 
     let output = Command::new("ls")
         // Tell the OS to record the command's output
@@ -28,7 +34,4 @@ fn main() {
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     stdout.lines().for_each(|line| print_line!(line));
-
-    let msg = "YAY".blue().yellow();
-    println!("{msg}");
 }
