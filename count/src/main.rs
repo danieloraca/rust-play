9f259e36400b1bbe98f4dc8ff610e78b54f04614
Dan <daniel.oraca@gmail.com>
2024-04-10 17:55:04 +0100

    Cleaned up redundant code, optimized arithmetic calculation, and added comments for clarity.

--- count/src/main.rs
+++ count/src/main.rs
@@ -1,12 +1,12 @@
-
-
 fn main() {
     //let sum: u64 = (0..100_000_000).into_par_iter().sum();
     //println!("Sum: {}", sum);
-    
+
     //let x: u64 = (0..100_000_000).sum();
     //let a:u64 = 100_000_000;
-    let x:u64 = 100000000*(100000000+1)/2;
+    let x: u64 = 100000000 * (100000000 + 1) / 2;
     println!("Hello, world! {}", x);
 
+    let x: u64 = 100000000 * (100000000 + 1) / 2;
+    println!("Hello, world! {}", x);
 }
