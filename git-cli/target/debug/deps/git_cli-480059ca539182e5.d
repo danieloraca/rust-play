56806af3407405229914113f75f142bc3249abc9
Dan <daniel.oraca@gmail.com>
2024-04-10 18:05:40 +0100

    cleanup

--- git-cli/target/debug/deps/git_cli-480059ca539182e5.d
+++
@@ -1,5 +0,0 @@
-/Users/danieloraca/Development/rust-play/git-cli/target/debug/deps/git_cli-480059ca539182e5: src/main.rs
-
-/Users/danieloraca/Development/rust-play/git-cli/target/debug/deps/git_cli-480059ca539182e5.d: src/main.rs
-
-src/main.rs:
--- git-cli/target/debug/git-cli.d
+++
@@ -1,1 +0,0 @@
-/Users/danieloraca/Development/rust-play/git-cli/target/debug/git-cli: /Users/danieloraca/Development/rust-play/git-cli/src/main.rs
