5f212035ec7a3f1ea0bb1a95a96e8d910e85707d
Dan <daniel.oraca@gmail.com>
2024-04-10 17:53:33 +0100

    tweaks on csv

--- csv-play/src/main.rs
+++ csv-play/src/main.rs
@@ -1,7 +1,7 @@
-use csv:: {Writer};
-use uuid::Uuid;
-use names::Generator;
+use csv::Writer;
 use micro_rand::*;
+use names::Generator;
+use uuid::Uuid;
 
 struct CsvRow<'a> {
     id: Uuid,
@@ -15,7 +15,8 @@ fn main() {
     let mut rnd = Random::new(1234);
 
     let path: &str = "person.csv";
-    let max_names: usize = 1_000_000;
+    let max_names: usize = 1_000;
+
     let mut csv_writer = Writer::from_path(path).unwrap();
     for _ in 0..max_names {
         let name: String = generator.next().unwrap();
@@ -25,12 +26,14 @@ fn main() {
             age: rnd.next_int_i32(18, 99) as u8,
             email: format!("{}@example.com", name.to_lowercase()),
         };
-        csv_writer.write_record([
-            &person.id.to_string(),
-            &person.name,
-            &person.age.to_string(),
-            &person.email,
-        ]).unwrap();
+        csv_writer
+            .write_record([
+                &person.id.to_string(),
+                &person.name,
+                &person.age.to_string(),
+                &person.email,
+            ])
+            .unwrap();
     }
 
     csv_writer.flush().unwrap();
@@ -48,7 +51,5 @@ mod tests {
             let record = result.unwrap();
             println!("{:?}", record);
         }
-
     }
 }
-
