40ea097dac0047e5fd63a1e685c020ec8a8746bd
Dan <daniel.oraca@gmail.com>
2024-04-12 20:18:27 +0100

    refactoring

--- git-cli/src/main.rs
+++ git-cli/src/main.rs
@@ -1,7 +1,7 @@
-use std::process::{Command, Stdio};
+use std::process::{Command, Output, Stdio};
 
 const CHATGPT_QUESTION: &str =
-    "From the above git diff describe the changes in less than 10 words as a commit message";
+    "Please make in less than 10 words a commit message from the above diff";
 
 fn fire_command(command_cli: &str, command_arguments: &str) -> String {
     let mut command: Command = Command::new(command_cli);
@@ -10,6 +10,18 @@ fn fire_command(command_cli: &str, command_arguments: &str) -> String {
     let command_result = command.stdout(Stdio::piped()).output().unwrap();
 
     String::from_utf8(command_result.stdout).unwrap()
+}
+
+fn talk_to_chatgpt(concatenated_lines: String) -> String {
+    let final_question: String = format!("{} {}", concatenated_lines.to_string(), CHATGPT_QUESTION);
+    println!("{final_question}");
+
+    let chat_response = fire_command(
+        "/Users/danieloraca/git-cli/rust-chatgpt-cli",
+        &final_question,
+    );
+
+    chat_response
 }
 
 fn read_git_diff() -> Vec<String> {
@@ -32,16 +44,11 @@ fn main() {
         std::process::exit(0);
     }
 
-    let final_question: String = format!("{} {}", concatenated_lines.to_string(), CHATGPT_QUESTION);
+    let chatgpt_commit_message = talk_to_chatgpt(concatenated_lines);
 
-    let mut chat_command: Command = Command::new("/Users/danieloraca/git-cli/rust-chatgpt-cli");
-    chat_command.arg(final_question);
+    println!("commit -am \"{}\"", chatgpt_commit_message.trim());
 
-    let result = chat_command.stdout(Stdio::piped()).output().unwrap();
-    let chatgpt_commit_comment: String = String::from_utf8(result.stdout).unwrap();
-    println!("commit -am \"{}\"", chatgpt_commit_comment.trim());
-
-    let commit_arg: String = format!("commit -am \"{}\"", chatgpt_commit_comment.trim());
+    let commit_arg: String = format!("commit -am \"{}\"", chatgpt_commit_message.trim());
 
     fire_command("/usr/bin/git", &commit_arg);
     fire_command("/usr/bin/git", "push");
