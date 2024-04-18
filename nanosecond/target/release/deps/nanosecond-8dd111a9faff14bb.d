24c9498bb17ef0142ec0deea52ef0bb712fc6d24
Dan <daniel.oraca@gmail.com>
2024-04-18 17:24:34 +0100

    update gitignore

---
+++ nanosecond/target/release/deps/nanosecond-8dd111a9faff14bb.d
@@ -0,0 +1,5 @@
+/Users/danieloraca/Development/rust-play/nanosecond/target/release/deps/nanosecond-8dd111a9faff14bb: src/main.rs
+
+/Users/danieloraca/Development/rust-play/nanosecond/target/release/deps/nanosecond-8dd111a9faff14bb.d: src/main.rs
+
+src/main.rs:
---
+++ nanosecond/target/release/nanosecond.d
@@ -0,0 +1,1 @@
+/Users/danieloraca/Development/rust-play/nanosecond/target/release/nanosecond: /Users/danieloraca/Development/rust-play/nanosecond/src/main.rs
