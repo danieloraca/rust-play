05da71f40d389512ec7ec2d00feb725d522b31e1
Dan <daniel.oraca@gmail.com>
2024-05-19 19:24:27 +0100

    use different rand crate for threads

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,6 +1,6 @@
 use csv::Writer;
-use micro_rand::*;
 use names::Generator;
+use rand::{self, Rng};
 use std::sync::{mpsc, Arc};
 use std::thread;
 use uuid::Uuid;
@@ -22,40 +22,47 @@ fn main() {
     let num_threads = 4;
     let max_rows = 1_000_000;
     let rows_per_thread = max_rows / num_threads;
+
     for _ in 0..num_threads {
         let tx = Arc::clone(&tx);
         thread::spawn(move || {
             let mut rows = Vec::with_capacity(rows_per_thread);
-            let mut r = Random::new(123475765765);
+            let mut rng = rand::thread_rng();
             let mut generator: Generator = Generator::default();
+
             for _ in 0..rows_per_thread {
                 let id = Uuid::new_v4();
-                let name: String = generator.next().unwrap();
-                let age = r.next_int_i32(18, 99) as u8;
-                let email = format!("{}@gmail.com", name.to_lowercase());
+                let name: String = generator.next().unwrap().replace(" ", "_");
+                let age = rng.gen_range(18..=99) as u8;
+                let email = format!("{}@example.com", name.to_lowercase());
+
                 let person = CsvRow {
                     id,
                     name,
                     age,
                     email,
                 };
-
                 rows.push(person);
             }
-            let _ = tx.send(rows);
+
+            tx.send(rows).expect("Could not send data!");
         });
     }
 
-    drop(tx);
+    drop(tx); // Close the sending end of the channel
 
     for received in rx {
         for record in received {
             writer
-                .write_record(&[format!(
-                    "{},{},{},{}",
-                    record.id, record.name, record.age, record.email
-                )])
+                .write_record(&[
+                    record.id.to_string(),
+                    record.name,
+                    record.age.to_string(),
+                    record.email,
+                ])
                 .expect("Cannot write record");
         }
     }
+
+    writer.flush().expect("Failed to flush the writer");
 }
