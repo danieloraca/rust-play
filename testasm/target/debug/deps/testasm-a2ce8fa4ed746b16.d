b7586f4e5aef7910fb0343d0c90eaceda8c3c358
Dan <daniel.oraca@gmail.com>
2024-05-23 20:39:54 +0100

    testasm

---
+++ testasm/target/debug/deps/testasm-a2ce8fa4ed746b16.d
@@ -0,0 +1,5 @@
+/Users/danieloraca/Development/rust-play/testasm/target/debug/deps/libtestasm-a2ce8fa4ed746b16.rmeta: src/main.rs
+
+/Users/danieloraca/Development/rust-play/testasm/target/debug/deps/testasm-a2ce8fa4ed746b16.d: src/main.rs
+
+src/main.rs:
---
+++ testasm/target/debug/deps/testasm-f05f6a864fed795e.d
@@ -0,0 +1,5 @@
+/Users/danieloraca/Development/rust-play/testasm/target/debug/deps/libtestasm-f05f6a864fed795e.rmeta: src/main.rs
+
+/Users/danieloraca/Development/rust-play/testasm/target/debug/deps/testasm-f05f6a864fed795e.d: src/main.rs
+
+src/main.rs:
