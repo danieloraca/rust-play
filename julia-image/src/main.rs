6e6fbf47801c1c12862400bc7a731f4850d27f1f
Dan <daniel.oraca@gmail.com>
2024-06-14 17:02:25 +0100

    cyan julia

--- julia-image/src/main.rs
+++ julia-image/src/main.rs
@@ -5,8 +5,8 @@ use rand::Rng;
 fn julia(c: Complex<f64>, x: f64, y: f64) -> u8 {
     let mut z = Complex::new(x, y);
 
-    for i in 0..255 {
-        if z.norm() > 2.0 {
+    for i in 20..255 {
+        if z.norm_squared() > 2 as f64 {
             return i as u8;
         }
         z = z * z + c;
@@ -24,15 +24,15 @@ fn main() {
     let c = Complex::new(-0.4, 0.6);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let x = x as f64 / width as f64 * 3.01 - 1.5;
+        let x = x as f64 / width as f64 * 3.1 - 1.5;
         let y = y as f64 / height as f64 * 2.9 - 1.5;
 
         let intensity = julia(c, x, y);
-        let red = intensity;
-        let green = rand::thread_rng().gen_range(0.0..1.0);
-        let blue = rand::thread_rng().gen_range(0.0..1.0);
+        let red = rand::thread_rng().gen_range(1..10);
+        let green = intensity;
+        let blue = intensity;
 
-        *pixel = Rgb([red, green as u8, blue as u8]);
+        *pixel = Rgb([red as u8, green as u8, blue]);
     }
 
     img.save("julia.png").unwrap();
