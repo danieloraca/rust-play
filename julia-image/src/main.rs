b66e121c02f226e12ed5b572d151dd6cab397d17
Dan <daniel.oraca@gmail.com>
2024-06-14 16:14:16 +0100

    julia tweaks

--- julia-image/src/main.rs
+++ julia-image/src/main.rs
@@ -1,5 +1,6 @@
 use image::{ImageBuffer, Rgb};
 use nalgebra::{Complex, Normed};
+use rand::Rng;
 
 fn julia(c: Complex<f64>, x: f64, y: f64) -> u8 {
     let mut z = Complex::new(x, y);
@@ -23,12 +24,15 @@ fn main() {
     let c = Complex::new(-0.4, 0.6);
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let x = x as f64 / width as f64 * 3.1 - 2.0;
+        let x = x as f64 / width as f64 * 3.01 - 1.5;
         let y = y as f64 / height as f64 * 2.9 - 1.5;
 
         let intensity = julia(c, x, y);
+        let red = intensity;
+        let green = rand::thread_rng().gen_range(0.0..1.0);
+        let blue = rand::thread_rng().gen_range(0.0..1.0);
 
-        *pixel = Rgb([intensity, intensity, intensity]);
+        *pixel = Rgb([red, green as u8, blue as u8]);
     }
 
     img.save("julia.png").unwrap();
